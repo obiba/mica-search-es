e28692321ea6f1a1bdd1e6463325c822db85bf88
Ramin Haeri Azad <rhaeri@maelstrom-reseach.org>
2024-04-08 16:23:19 -0400

    Changed POM and few bug fixed for java21

--- src/test/java/org/obiba/es/mica/support/ESHitSourceMapHelperTest.java
+++ src/test/java/org/obiba/es/mica/support/ESHitSourceMapHelperTest.java
@@ -10,9 +10,9 @@
 
 package org.obiba.es.mica.support;
 
-import org.assertj.core.util.Maps;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.Assert.assertNotNull;
@@ -22,9 +22,9 @@ public class ESHitSourceMapHelperTest {
 
   @Test
   public void flattenMapSimple() {
-    Map<String, Object> map = Maps.newHashMap();
+    Map<String, Object> map = new HashMap<>();
     map.put(("A"), "bla");
-    Map<String, String> flattened = Maps.newHashMap();
+    Map<String, String> flattened = new HashMap<>();
     ESHitSourceMapHelper.flattenMap(map, flattened);
     assertTrue(map.size() == flattened.size());
     assertTrue(map.get("A").equals(flattened.get("A")));
@@ -32,14 +32,14 @@ public class ESHitSourceMapHelperTest {
 
   @Test
   public void flattenMapOneHierarchy() {
-    Map<String, Object> mapB = Maps.newHashMap();
+    Map<String, Object> mapB = new HashMap<>();
     mapB.put("B", "bla");
 
-    Map<String, Object> mapA = Maps.newHashMap();
+    Map<String, Object> mapA = new HashMap<>();
     mapA.put("A", "blabla");
     mapA.put("A1", mapB);
 
-    Map<String, String> flattened = Maps.newHashMap();
+    Map<String, String> flattened = new HashMap<>();
     ESHitSourceMapHelper.flattenMap(mapA, flattened);
     assertTrue(flattened.size() == 2);
     assertNotNull(flattened.get("A"));
@@ -50,21 +50,21 @@ public class ESHitSourceMapHelperTest {
 
   @Test
   public void flattenMapSeveralHierarchy() {
-    Map<String, Object> mapD = Maps.newHashMap();
+    Map<String, Object> mapD = new HashMap<>();
     mapD.put("D", "blabla");
 
-    Map<String, Object> mapC = Maps.newHashMap();
+    Map<String, Object> mapC = new HashMap<>();
     mapC.put("C", mapD);
 
-    Map<String, Object> mapB = Maps.newHashMap();
+    Map<String, Object> mapB = new HashMap<>();
     mapB.put("B", mapC);
 
-    Map<String, Object> mapA = Maps.newHashMap();
+    Map<String, Object> mapA = new HashMap<>();
     mapA.put("A", mapB);
     mapA.put("A1", mapC);
     mapA.put("A2", mapD);
 
-    Map<String, String> flattened = Maps.newHashMap();
+    Map<String, String> flattened = new HashMap<>();
     ESHitSourceMapHelper.flattenMap(mapA, flattened);
     assertTrue(flattened.size() == 3);
     assertTrue(flattened.get("A.B.C.D").equals("blabla"));
